42454fafc91d108cdb4bb73fc11af2689490d91b
EvelynOct <evelyne.aritonang72@gmail.com>
2026-06-15 03:11:37 +0700

    Implement publish function in Product service and Product controller.

--- src/controller/product.rs
+++ src/controller/product.rs
@@ -5,7 +5,6 @@ use bambangshop::Result;
 use crate::model::product::Product;
 use crate::service::product::ProductService;
 
-
 #[post("/", data = "<product>")]
 pub fn create(product: Json<Product>) -> Result<Created<Json<Product>>> {
     return match ProductService::create(product.into_inner()) {
@@ -33,6 +32,14 @@ pub fn read(id: usize) -> Result<Json<Product>> {
 #[delete("/<id>")]
 pub fn delete(id: usize) -> Result<Json<Product>> {
     return match ProductService::delete(id) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    };
+}
+
+#[post("/<id>/publish")]
+pub fn publish(id: usize) -> Result<Json<Product>> {
+    return match ProductService::publish(id) {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
     };
